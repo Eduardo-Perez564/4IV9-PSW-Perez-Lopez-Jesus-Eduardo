f4d88e01c142aaecbc5f3294d1751723c6134d45
Eduardo-Perez564 <jperezl1902@alumno.ipn.mx>
2021-04-09 01:47:34 -0500

    script problma 10

---
+++ JS/problema10.js
@@ -0,0 +1,46 @@
+function validarn(e){
+    var teclado = (document.all)?e.keycode:e.which;
+
+    if(teclado == 8) return true;
+
+    var patron = /[0-9\d.]/;
+
+    if(teclado == 8) return true;
+
+    var prueba = String.fromCharCode(teclado);
+
+    return patron.test(prueba);
+}
+
+function Calcular_Utilidades(){
+    var Salario = parseFloat(document.formulario.salario.value);
+    var Antiguedad = parseFloat(document.formulario.tiempo.value);
+    var Utilidades;
+
+    if(document.formulario.salario.value == "" || document.formulario.tiempo.value == ""){
+        alert("Por favor, no deje campos vacíos");
+    }else{
+        if(Antiguedad < 1){
+            Utilidades = Salario*.05;
+            document.formulario.utilidades.value = "$"+Utilidades;
+        }else if(Antiguedad >= 1 && Antiguedad < 2){
+            Utilidades = Salario*.07;
+            document.formulario.utilidades.value = "$"+Utilidades;
+        }else if(Antiguedad >= 2 && Antiguedad < 5){
+            Utilidades = Salario*.10;
+            document.formulario.utilidades.value = "$"+Utilidades;
+        }else if(Antiguedad >= 5 && Antiguedad < 10){
+            Utilidades = Salario*.15;
+            document.formulario.utilidades.value = "$"+Utilidades;
+        }else{
+            Utilidades = Salario*.20;
+            document.formulario.utilidades.value = "$"+Utilidades;
+        }
+    }
+}
+
+function borrar(){
+    document.formulario.utilidades.value = "";
+    document.formulario.salario.value = "";
+    document.formulario.tiempo.value = "";
+}
